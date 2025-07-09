31db66e5f06037c9640df5ba3e7463cbf62b87a5
ZicBusiness <77104285+ZicBusiness@users.noreply.github.com>
2025-07-09 13:25:04 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
   resolve: {
@@ -11,4 +10,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: '/DEV-TEST-ENVIRONMENT/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
